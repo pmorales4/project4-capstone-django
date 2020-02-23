373a000c6a09073647c1853a5851bb1bc5e0126f
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-02-22 21:03:37 -0500

    fix whitenoise

--- music_project/settings.py
+++ music_project/settings.py
@@ -154,4 +154,4 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
