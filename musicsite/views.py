860322170d6d337a10d32cb4cfcee80fc5df4696
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-02-24 18:14:00 -0500

    stage heroku commit agaiN 7

--- musicsite/views.py
+++ musicsite/views.py
@@ -35,7 +35,7 @@ def artist_create(request):
             return redirect('artist_detail', pk=artist.pk)
     else:
         form = ArtistForm()
-    return render(request, 'musicsite/artist_form.html', {'form': form})
+    return render(request, 'musicsite/home.html', {'form': form})
 
 # Edit an artist on site 
 def artist_edit(request, pk):
