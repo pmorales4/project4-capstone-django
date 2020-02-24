742217cdc90dca39ebb8877ace162870e7ae4002
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-02-24 11:34:58 -0500

    stage heroku commit agaiN 3

--- music_project/settings.py
+++ music_project/settings.py
@@ -143,14 +143,14 @@ LOGIN_URL = 'login'
 STATIC_URL = '/static/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-     '/var/www/static/',
+    os.path.join(BASE_DIR, 'static'),
+    
 )
 
 #  Add configuration for static files storage using whitenoise
@@ -159,3 +159,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+#************************************************
+
+
+
