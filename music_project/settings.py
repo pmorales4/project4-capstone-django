eb5a65d3a54268ca5873c6cd54394be7189349f2
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-02-24 13:29:22 -0500

    stage heroku commit agaiN 6

--- music_project/settings.py
+++ music_project/settings.py
@@ -150,10 +150,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/musicsite/static'),
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, '/musicsite/static'),
     
-)
+# )
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
