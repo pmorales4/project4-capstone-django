5c245d9b81ab8e658bc5fbbd8df930ce8dd8a6aa
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-02-25 10:29:59 -0500

    ll

--- musicsite/views.py
+++ musicsite/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Artist
-from .forms import ArtistForm
+from .models import *
+from .forms import *
+from django.http import HttpResponseForbidden
 
 # Create your views here.
 # logic for routes in this page
@@ -27,30 +28,53 @@ def artist_detail(request, pk):
     return render(request, 'musicsite/artist_detail.html', {'artist': artist})
     
 # CREATE AN ARTIST ON FRONT END SECTION 
-def artist_create(request):
-    if request.method == 'POST':
-        form = ArtistForm(request.POST, request.FILES)
-        if form.is_valid():
-            artist =form.save()
-            return redirect('artist_detail', pk=artist.pk)
-    else:
-        form = ArtistForm()
-    return render(request, 'musicsite/artist_form.html', {'form': form})
+# def artist_create(request):
+#     if request.method == 'POST':
+#         form = ArtistForm(request.POST, request.FILES)
+#         # import pdb; pdb.set_trace()
+#         if form.is_valid():
+#             artist =form.save()
+#             return redirect('artist_detail', pk=artist.pk)
+#     else:
+#         form = ArtistForm()
+#     return render(request, 'musicsite/artist_form.html', {'form': form})
 
 # Edit an artist on site 
-def artist_edit(request, pk):
-    artist = Artist.objects.get(pk=pk)
-    if request.method == "POST":
-        form = ArtistForm(request.POST, request.FILES, instance=artist)
-        if form.is_valid():
-            artist = form.save()
-            return redirect('artist_detail', pk=artist.pk)
-    else:
-        form = ArtistForm(instance=artist)
-    return render(request, 'musicsite/artist_form.html', {'form': form})
+# def artist_edit(request, pk):
+#     artist = Artist.objects.get(pk=pk)
+#     if request.method == "POST":
+#         form = ArtistForm(request.POST, request.FILES, instance=artist)
+#         if form.is_valid():
+#             artist = form.save()
+#             return redirect('artist_detail', pk=artist.pk)
+#     else:
+#         form = ArtistForm(instance=artist)
+#     return render(request, 'musicsite/artist_form.html', {'form': form})
 
 
 # Deletes an Artist 
 def artist_delete(request, pk):
     Artist.objects.get(id=pk).delete()
     return redirect('artist_list')
+
+
+def artist_create(request):
+    if request.method == 'POST':
+        #import pdb; pdb.set_trace()
+        form = ArtistForm(request.POST, request.FILES)
+        if form.is_valid:
+            artists = form.save()
+            return redirect('artists_detail', id = artists.id)
+    else:
+        form = ArtistForm()
+        return render(request, 'artists_form.html', {'form': form})
+def artists_update(request, id):
+    artists = artists.objects.get(id = id)
+    if request.method == 'POST':
+        form = ArtistForm(request.POST, request.FILES, instance = artists)
+        if form.is_valid:
+            artists = form.save()
+            return redirect('artists_detail', id = artists.id)
+    else:
+        form = ArtistForm(instance = artists)
+        return render(request, 'artists_form.html', {'form': form})
