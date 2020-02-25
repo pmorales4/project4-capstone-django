5da355b6a40b99d16b29e15c12ce95089a838c75
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-02-24 22:11:26 -0500

    stage heroku commit agaiN 7

--- musicusers/models.py
+++ musicusers/models.py
@@ -11,12 +11,12 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
 # Pillow keeps images at a certain size for reloaded on browser
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
